9016891fb92da3e6c7abccb6a82ffb8e9452c6f0
letqin <86627655+letqin@users.noreply.github.com>
2021-08-29 11:28:58 -0400

    pseudo coded javascript

---
+++ 02-Homework/code/script.js
@@ -0,0 +1,17 @@
+// when I click the start button
+    // a timer starts
+
+// the first question is presented
+    // if the answer is correct, a point is added to the player score
+    // if the answer is incorrect, time is subtracted
+
+// the next question is presented to the user
+
+// the quiz is over if
+    // all of the questions are answered
+    // or
+    // time runs out
+
+// asks for initials
+
+// saves initials and high score in local storage
