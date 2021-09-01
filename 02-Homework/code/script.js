c3919cd7845b0018344e0740d3bc9c75267a0240
letqin <86627655+letqin@users.noreply.github.com>
2021-09-01 00:08:53 -0400

    added first question and date

--- 02-Homework/code/script.js
+++ 02-Homework/code/script.js
@@ -1,15 +1,65 @@
 var questions = [
+
     {
-        question: "What are the similarities between Java and JavaScript?";
-        options: ""
+        question: "What are the similarities between Java and JavaScript?",
+        options: ["They are the same thing.", "Java is a front end framework, JavaScript is a database.", "Nothing, don't be ridiculous.", "They both require coffee in the boilerplate."],
+        answer: "Nothing, don't be ridiculous."
     },
+
+    // {
+    //     question: 
+    //     options: 
+    //     answer: 
+    // },
+
+    // {
+    //     question: 
+    //     options: 
+    //     answer: 
+    // },
+
+    // {
+    //     question: 
+    //     options: 
+    //     answer: 
+    // },
+
+    // {
+    //     question: 
+    //     options: 
+    //     answer: 
+    // },
+
+    // {
+    //     question: 
+    //     options: 
+    //     answer: 
+    // },
+
+    // {
+    //     question: 
+    //     options: 
+    //     answer: 
+    // },
 ];
 
 // when I click the start button
+
 start.onclick = {
     
-}
-    // a timer starts
+};
+
+// var t = d.getTime();
+
+// a timer starts
+
+var minutes = 1000 * 60;
+var hours = minutes * 60;
+var days = hours * 24;
+var years = days * 365;
+var d = new Date();
+
+console.log(d);
 
 // the first question is presented
     // if the answer is correct, a point is added to the player score
