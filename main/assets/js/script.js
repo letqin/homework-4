5450008c9736ca768d0c22ee4b9eca0a99203ff1
Quinn Ryan <letqin@gmail.com>
2022-01-12 00:53:30 -0500

    no worky

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -1,14 +1,28 @@
+// questions and answer options
+let question = document.querySelector("#question");
+let optionA = document.querySelector("#a");
+let optionB = document.querySelector("#b");
+let optionC = document.querySelector("#c");
+let optionD = document.querySelector("#d");
+let startQuiz = document.getElementById("start");
+// let nextQuestion = document.querySelector("next-question");
+
+// wins, losses, timer, and start button
 let win = document.querySelector(".win");
 let loss = document.querySelector(".loss");
 let timer = document.querySelector(".timer-count");
 const start = document.querySelector(".start-button");
 
+// variables for running the quiz
+let questionIndex = 0;
+let optionIndex = 0;
 let correct = 0;
 let incorrect = 0;
 let score = 0;
 let timer = 60;
 let timerCount;
 
+// question array
 let questions = [
     {
         question: "What is an array?",
@@ -64,11 +78,11 @@ let questions = [
         options: [
             "They are used to modify data within an array",
 
-            "Studying computer science",
+            "The act of studying computer science",
 
-            "",
+            "Light refraction in JavaScript",
 
-            "",  
+            "An alternative to an ES5 array",  
         ],
 
         answer: "They are used to modify data within an array",
@@ -126,11 +140,11 @@ let questions = [
         question: "Why would you console.log something?",
 
         options: [
-            "",
+            "This is the only way JavaScript can use information",
 
-            "",
+            "There is no reason to ever console log as it is depreciated",
 
-            "",
+            "This shows the information to the user in the DOM",
 
             "To find it's value during the coding or debugging process",  
         ],
@@ -139,37 +153,79 @@ let questions = [
     },
 
     {
-        question: "",
+        question: "What is ECMAScript?",
 
         options: [
-            "",
+            "A programming language",
 
-            "",
+            "The uniform standard for writing JavaScript",
 
-            "",
+            "Introduced to ensure interoperability between web pages",
 
-            "",  
+            "All of the above",  
         ],
 
-        answer: "",
+        answer: "All of the above",
     },
 
     {
-        question: "",
+        question: "What is a loop?",
 
         options: [
-            "",
+            "The act of going through an operation multiple times",
 
-            "",
+            "Broken code",
 
-            "",
+            "A font ligature in Fira Code",
 
-            "",  
+            "A virtual round-about where tiny cars have traffic jams",  
         ],
 
-        answer: "",
+        answer: "The act of going through an operation multiple times",
     }
-]
+];
+
+let startTime = 120;
+
+// timer function
+let timer = () => {
+    startTimer
+    setInterval(() => {
+        if (startTimer === 0) {
+            document.querySelector("#timer-display").innerHTML = "Pencils down!";
+            clearInterval(startTimer);
+        } else {
+            startTimer--
+            let minutes = Math.floor(startTimer / 60);
+            let seconds = startTimer - minutes * 60;
+            document.querySelector1("#timer-display").innerHTML =
+            "Time Left: " + minutes + ":" + seconds;
+        }
+    }, 1000);
+};
+
+startQuiz.addEventListener("click", (e) => {
+    e.preventDefault();
+    timer();
+    let questions = [...questions[questionIndex].options];
+    if (e.target.tagName === "BUTTON") {
+        question.innerHTML = questions[questionIndex].question
+        optionA.innerText = optionIndex[0];
+        optionB.innerText = optionIndex[1];
+        optionC.innerText = optionIndex[2];
+        optionD.innerText = optionIndex[3];
+    }
+});
+
+// let quizIndexControl = () => {
+//     if (quizIndex >= 9) {
+//         showHighScores();
+//     } else {
+//         questionIndex++;
+//     }
+
+//     let shuffle
+// }
 
 // called when the page starts up to show previous high scores
 function init() {
@@ -201,18 +257,4 @@ function incorrect () {
     // display the next question
 };
 
-function startTimer() {
-    timer = setInterval(function() {
-        timerCount--;
-        timerElement.textContent = timerCount;
-        if (timerCount <= 0) {
-            if (timerCount === 0) {
-                // clears the timer
-                clearInterval(timer);
-                console.log(timer);
-                console.log(timerCount);
-            };
-        };
-    }, 1000);
-};
 
